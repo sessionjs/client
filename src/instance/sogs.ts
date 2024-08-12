0433a231b005360afa18785c8f3f5b9fe32b6efd
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-08-13 02:48:43 +0400

    Add blindedSessionId to encodeSogsMessage

--- src/instance/sogs.ts
+++ src/instance/sogs.ts
@@ -1,7 +1,7 @@
 import type { Session } from '@/instance'
-import sodium from 'libsodium-wrappers-sumo'
+import sodium, { to_hex } from 'libsodium-wrappers-sumo'
 import { v4 as uuid } from 'uuid'
-import { hexToUint8Array, isHex, Uint8ArrayToBase64 } from '@/utils'
+import { hexToUint8Array, Uint8ArrayToBase64 } from '@/utils'
 import { VisibleMessage, type AttachmentPointerWithUrl } from '@/messages/schema/visible-message'
 import { MAX_ATTACHMENT_FILESIZE_BYTES } from '@session.js/consts'
 import { SessionRuntimeError, SessionRuntimeErrorCode, SessionValidationError, SessionValidationErrorCode } from '@session.js/errors'
@@ -9,15 +9,12 @@ import type { Keypair } from '@session.js/keypair'
 import { sign } from 'curve25519-js'
 import type { KeyPair } from 'libsodium-wrappers-sumo'
 
-export function encodeSogsMessage(this: Session, { serverPk, to, text, attachments }: {
+export function encodeSogsMessage(this: Session, { serverPk, text, attachments }: {
   serverPk: string
-  to: string
   text?: string
   attachments?: File[]
-}): { data: string, signature: string } {
+}): { data: string, signature: string, blindedSessionId: string } {
   if (!this.sessionID || !this.keypair) throw new SessionRuntimeError({ code: SessionRuntimeErrorCode.EmptyUser, message: 'Instance is not initialized; use setMnemonic first' })
-  if (to.length !== 66) throw new SessionValidationError({ code: SessionValidationErrorCode.InvalidSessionID, message: 'Invalid session ID length' })
-  if (!to.startsWith('15') || !isHex(to)) throw new SessionValidationError({ code: SessionValidationErrorCode.InvalidSessionID, message: 'Session ID must be a hex string starting from 15' })
   if (attachments?.some(a => !(a instanceof File))) throw new SessionValidationError({ code: SessionValidationErrorCode.InvalidAttachment, message: 'Attachments must be instances of File' })
   if (attachments?.some(a => a.size > MAX_ATTACHMENT_FILESIZE_BYTES)) throw new SessionValidationError({ code: SessionValidationErrorCode.InvalidAttachment, message: 'Attachment size exceeds the limit: ' + MAX_ATTACHMENT_FILESIZE_BYTES + ' bytes' })
 
@@ -39,13 +36,21 @@ export function encodeSogsMessage(this: Session, { serverPk, to, text, attachmen
   const paddedBody = addMessagePadding(msg.plainTextBuffer())
   const data = Uint8ArrayToBase64(paddedBody)
 
+
+  const blindedKeyPair = getBlindingValues(
+    hexToUint8Array(serverPk),
+    this.keypair.ed25519
+  )
+
   const signature = getSignatureWithBlinding({
     data: paddedBody,
     keypair: this.keypair,
-    serverPk
+    blindedKeyPair
   })
+
+  const blindedSessionId = '05' + to_hex(blindedKeyPair.publicKey)
   
-  return { data, signature }
+  return { data, signature, blindedSessionId }
 }
 
 export function addMessagePadding(messageBuffer: Uint8Array): Uint8Array {
@@ -76,16 +81,15 @@ export async function getSignature(data: Uint8Array, keypair: Keypair): Promise<
   return base64Sig
 }
 
-function getSignatureWithBlinding({ data, keypair, serverPk }: {
+function getSignatureWithBlinding({ data, keypair, blindedKeyPair }: {
   data: Uint8Array, 
-  serverPk: string,
+  blindedKeyPair: {
+    a: Uint8Array
+    secretKey: Uint8Array
+    publicKey: Uint8Array
+  },
   keypair: Keypair
 }): string {
-  const blindedKeyPair = getBlindingValues(
-    hexToUint8Array(serverPk),
-    keypair.ed25519
-  )
-
   const signature = getSogsSignature({
     blinded: true,
     ka: blindedKeyPair.secretKey,
